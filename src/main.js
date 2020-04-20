a1811a0e383c1792ee6ce9cad97ea3855004cc60
tganyan <tganyan@gmail.com>
2020-04-20 11:10:13 -0700

    Add collision detection for bricks, make brick disappear on impact

--- src/main.js
+++ src/main.js
@@ -34,12 +34,12 @@ const brickOffsetLeft = 30;
 for (let c = 0; c < brickColumnCount; c++) {
 	bricks[c] = [];
 	for (let r = 0; r < brickRowCount; r++) {
-		bricks[c][r] = { x: 0, y: 0 };
+		bricks[c][r] = { x: 0, y: 0, status: 1 };
 	}
 }
 
 // Functions
-const drawBall = () => {
+const drawBall = (fill) => {
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
 	ctx.fillStyle = '#0095dd';
@@ -58,15 +58,17 @@ const drawPaddle = () => {
 const drawBricks = () => {
 	for (let c = 0; c < brickColumnCount; c++) {
 		for (let r = 0; r < brickRowCount; r++) {
-			const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-			const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-			bricks[c][r].x = brickX;
-			bricks[c][r].y = brickY;
-			ctx.beginPath();
-			ctx.rect(brickX, brickY, brickWidth, brickHeight);
-			ctx.fillStyle = '#0095dd';
-			ctx.fill();
-			ctx.closePath();
+			if (bricks[c][r].status === 1) {
+				const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+				const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeight);
+				ctx.fillStyle = '#0095dd';
+				ctx.fill();
+				ctx.closePath();
+			}
 		}
 	}
 }
@@ -76,6 +78,7 @@ const draw = () => {
 	drawBricks();
 	drawBall();
 	drawPaddle();
+	collisionDetection();
 	x += dx;
 	y += dy;
 
@@ -90,8 +93,8 @@ const draw = () => {
 		if (x > paddleX && x < paddleX + paddleWidth) {
 			dy = -(dy + 1);
 		} else {
-			// alert('GAME OVER, MAN! GAME OVER!!!');
-			// document.location.reload();
+			alert('GAME OVER, MAN! GAME OVER!!!');
+			document.location.reload();
 			clearInterval(interval);
 		}
 	}
@@ -124,6 +127,21 @@ const keyUpHandler = (event) => {
 		rightPressed = false;
 	} else if (event.key === 'Left' || event.key === 'ArrowLeft') {
 		leftPressed = false;
+	}
+}
+
+const collisionDetection = () => {
+	for (let c = 0; c < brickColumnCount; c++) {
+		for (let r = 0; r < brickRowCount; r ++) {
+			let b = bricks[c][r];
+
+			if (b.status === 1) {
+				if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+					dy = - dy;
+					b.status = 0;
+				}
+			}
+		}
 	}
 }
 
